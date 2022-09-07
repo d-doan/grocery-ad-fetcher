53e5ecd26376a6c34bda2f39aa42d0a176bc7160
d-doan <44249543+d-doan@users.noreply.github.com>
2022-09-07 16:40:02 -0700

    Add delete old files and ran this week's ads

--- contact_users/grocery_webscraper.py
+++ contact_users/grocery_webscraper.py
@@ -5,7 +5,6 @@ from datetime import date, timedelta
 import os
 
 # prob should chuck each grocery store in its own class so then can do it all in one file
-    
 
 def download_file(download_url, store_name, date, file_type):
     file_location = 'contact_users\\static\\contact_users\\Weekly_ads' + '\\' + date + '\\' + store_name + '.' + file_type
@@ -14,7 +13,7 @@ def download_file(download_url, store_name, date, file_type):
 def get_last_wednesday():
     todays_date = date.today()
     date_offset = (todays_date.weekday() -2) % 7
-    return str(todays_date - timedelta(days=date_offset))
+    return todays_date - timedelta(days=date_offset)
 
 class Hmart:
     driver = webdriver.Chrome(executable_path="C:/Users/dando/Downloads/chromedriver_win32/chromedriver.exe")
@@ -24,19 +23,21 @@ class Hmart:
 
     image_class = driver.find_element(By.XPATH, '//*[@id="maincontent"]/div[2]/div/div[3]/div[2]/div/div[1]/div[1]/img[1]')
     hmart_weekly_ad = image_class.get_attribute('src')
-    print("Hmart Weekly Ad:")
-    print(hmart_weekly_ad)
 
-    last_wednesday = get_last_wednesday()
-    dirName = 'contact_users\\static\\contact_users\\Weekly_ads' + '\\' + last_wednesday
+    last_wednesday = str(get_last_wednesday())
+    del_wed = str(get_last_wednesday() - timedelta(7))
+    
+    dir_name = 'contact_users\\static\\contact_users\\Weekly_ads' + '\\' + last_wednesday
+    prev_dir_name = 'contact_users\\static\\contact_users\\Weekly_ads' + '\\' + del_wed + '\\Hmart.jpg'
 
-    if not os.path.exists(dirName):
-        os.mkdir(dirName)
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
         download_file(hmart_weekly_ad, "Hmart", last_wednesday, "jpg")
-        print("wow we made a folder")
     else:
         download_file(hmart_weekly_ad, "Hmart", last_wednesday, "jpg")
-        print("alr exists")
+        
+    if os.path.exists(prev_dir_name):
+        os.remove(prev_dir_name)
 
 
 class Ralphs:
@@ -48,18 +49,20 @@ class Ralphs:
     # finds html element containing advertisement
     pdf_class = driver.find_element(By.LINK_TEXT, 'Download PDF')
     ralphs_weekly_ad = pdf_class.get_attribute('href')
-
-    print("Link for Ralph's Weekly Ad:")
-    print(ralphs_weekly_ad)
     
-    last_wednesday = get_last_wednesday()
-    dirName = 'contact_users\\static\\contact_users\\Weekly_ads' + '\\' + last_wednesday
+    last_wednesday = str(get_last_wednesday())
+    del_wed = str(get_last_wednesday() - timedelta(7))
+    
+    dir_name = 'contact_users\\static\\contact_users\\Weekly_ads' + '\\' + last_wednesday
+    prev_dir_name = 'contact_users\\static\\contact_users\\Weekly_ads' + '\\' + del_wed + '\\Ralphs.pdf'
+
 
     # checks if weekly directory exists yet, if not then make it
-    if not os.path.exists(dirName):
-        os.mkdir(dirName)
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
         download_file(ralphs_weekly_ad, "Ralphs", last_wednesday, "pdf")
-        print("wow we made a folder")
     else:
         download_file(ralphs_weekly_ad, "Ralphs", last_wednesday, "pdf")
-        print("alr exists")
+
+    if os.path.exists(prev_dir_name):
+        os.remove(prev_dir_name)
